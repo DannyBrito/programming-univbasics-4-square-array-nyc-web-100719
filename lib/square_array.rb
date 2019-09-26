2b098e32fc209a4689c5798576b09290790f2715
Danny Brito <britodanny1997@gmail.com>
2019-09-26 16:58:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
   # your code here
+  temp_array = Array.new
+  index = 0
+  while index < array.length do
+   temp_array << Math.sqrt(array[index])
+  end
+  temp_array
 end
